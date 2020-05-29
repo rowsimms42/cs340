67516229a53384d320c6a7829d76971936f56c72
Rowan Simmons <59707215+rowsimms42@users.noreply.github.com>
2020-05-29 17:25:01 -0400

    Update dbcon.js

--- dbcon.js
+++ dbcon.js
@@ -2,8 +2,9 @@ var mysql = require('mysql');
 var pool = mysql.createPool({
   connectionLimit : 10,
   host            : 'classmysql.engr.oregonstate.edu',
-  user            : 'cs340_wolfordj',
-  password        : '7235',
-  database        : 'cs340_wolfordj'
+  user            : 'cs340_simmonli',
+  password        : '6398',
+  database        : 'cs340_simmonli'
 });
+
 module.exports.pool = pool;

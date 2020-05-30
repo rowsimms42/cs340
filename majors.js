65998e0e0d8e9b4e3e4f6193863898c34d73632e
mastja <56243990+mastja@users.noreply.github.com>
2020-05-29 18:19:36 -0600

    Added functions for UPDATE functionality

--- majors.js
+++ majors.js
@@ -1,4 +1,4 @@
- module.exports = function(){
+module.exports = function(){
     var express = require('express');
     var router = express.Router();
 
@@ -13,6 +13,21 @@
         });
     }
 
+    function getMajor(res, mysql, context, major_id, complete){
+        var sql = "SELECT major_id, major_name, dept_chair, required_units FROM Majors WHERE major_id = ?";
+        var inserts = [major_id];
+        mysql.pool.query(sql, inserts, function(error, results, fields){
+            if(error){
+                res.write(JSON.stringify(error));
+                res.end();
+            }
+            context.major = results[0];
+            complete();
+        });
+    }
+
+    /*Display all majors. Requires web based javascript to delete users with AJAX*/
+
     router.get('/', function(req, res){
         var callbackCount = 0;
         var context = {};
@@ -26,6 +41,25 @@
         }
     });
 
+    /* Display one major for the specific purpose of updating majors */
+
+    router.get('/:major_id', function(req, res){
+        callbackCount = 0;
+        var context = {};
+        context.jsscripts = ["updatemajor.js"];
+        var mysql = req.app.get('mysql');
+        getMajor(res, mysql, context, complete);
+        function complete(){
+            callbackCount++;
+            if(callbackCount >= 1){
+                res.render('update-major', context);
+            }
+
+        }
+    });
+
+    /* Adds a major, redirects to the majors page after adding */
+
     router.post('/', function(req, res){
         console.log(req.body)
         var mysql = req.app.get('mysql');
@@ -38,6 +72,23 @@
                 res.end();
             } else {
                 res.redirect('/majors');
+            }
+        });
+    });
+
+    /* The URI that update data is sent to in order to update a major */
+
+    router.put('/:major_id', function(req, res){
+        var mysql = req.app.get('mysql');
+        var sql = "UPDATE Majors SET major_name=?, dept_chair=?, required_units=? WHERE major_id=?";
+        var inserts = [req.body.major_name, req.body.dept_chair, req.body.required_units, req.params.major_id];
+        sql = mysql.pool.query(sql,inserts,function(error, results, fields){
+            if(error){
+                res.write(JSON.stringify(error));
+                res.end();
+            }else{
+                res.status(200);
+                res.end();
             }
         });
     });
